2e178d5c454b6cf9825430ccdeaf82b9b754da99
crhino <piraino.chris@gmail.com>
2015-09-13 01:05:43 -0700

    Add metrics thread to main
    - Configurable registration time and metrics endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![feature(custom_derive, plugin)]
 #![plugin(serde_macros)]
+#![feature(convert, result_expect)]
+#![plugin(docopt_macros)]
 
 extern crate iron;
 extern crate router;
@@ -10,6 +12,22 @@ extern crate serde_json;
 extern crate time;
 
 extern crate transit;
+extern crate ferrous_threads;
+
+extern crate rustc_serialize;
+extern crate docopt;
+
+// Write the Docopt usage string with the `docopt!` macro.
+docopt!(Config, "
+Usage: translucence [options] <metrics-address>
+
+    Run the translucence process with specified parameters.
+
+Options:
+    -r SECONDS  The registration interval [default: 10]
+");
+
+
 
 mod proc_fs;
 mod metrics_sender;
@@ -18,8 +36,37 @@ use proc_fs::stats::*;
 use proc_fs::kernel::*;
 use proc_fs::net::*;
 use proc_fs::ToPid;
+use metrics_sender::*;
 use iron::{Iron, IronResult, Request, Response};
 use router::Router;
+
+use ferrous_threads::thread_pool::{ThreadPool};
+use std::error::Error;
+use std::str::FromStr;
+
+fn main() {
+    let config: Config = Config::docopt().decode().unwrap_or_else(|e| e.exit());
+    let mut pool = ThreadPool::new(2, 2);
+
+    let  router_thread = pool.thread().expect("Could not request router thread");
+    router_thread.start(Box::new(move || {
+        let mut router = Router::new();
+        router.get("/proc/:pid/statm", proc_statm_handler);
+        router.get("/proc/:pid/io", proc_io_handler);
+        router.get("/proc/:pid/stack", proc_stack_handler);
+        router.get("/net/tcpstats", proc_tcp_handler);
+
+        Iron::new(router).http("localhost:3000").unwrap();
+    })).expect("Could not start router thread");
+
+    let  metrics_thread = pool.thread().expect("Could not request metrics thread");
+    metrics_thread.start(Box::new(move || {
+        metrics_reporting(config);
+    })).expect("Could not start metrics thread");
+
+    router_thread.join().expect("Failed to join router thread");
+    metrics_thread.join().expect("Failed to join metrics thread");
+}
 
 fn proc_statm_handler(req: &mut Request) -> IronResult<Response> {
     let ref pid = req.extensions.get::<Router>().unwrap().find("pid").unwrap_or("/");
@@ -48,12 +95,29 @@ fn proc_tcp_handler(_req: &mut Request) -> IronResult<Response> {
     Ok(Response::with(serialized))
 }
 
-fn main() {
-    let mut router = Router::new();
-    router.get("/proc/:pid/statm", proc_statm_handler);
-    router.get("/proc/:pid/io", proc_io_handler);
-    router.get("/proc/:pid/stack", proc_stack_handler);
-    router.get("/net/tcpstats", proc_tcp_handler);
+fn metrics_reporting(config: Config) {
+    let mut metrics_sender = MetricSender::new("0.0.0.0:0", String::from("translucence"))
+        .expect("Could not create metric sender");
+    let metric_addr = config.arg_metrics_address.as_str();
+    let publish_interval = match u32::from_str(config.flag_r.as_str()) {
+        Ok(n) => n,
+        Err(_) => 10, // Default to 10 seconds
+    };
 
-    Iron::new(router).http("localhost:3000").unwrap();
+    loop {
+        let tcp = match process_tcp() {
+            Ok(t) => t,
+            Err(ref e) => {
+                println!("Error getting tcp info: {}", e.description());
+                continue
+            },
+        };
+
+        let data = Metric::Network(tcp);
+        match metrics_sender.send_to(data, metric_addr) {
+            Ok(_) => {},
+            Err(ref e) => println!("Error sending metrics: {}", e.description()),
+        }
+        ::std::thread::sleep_ms(publish_interval * 1000);
+    }
 }
--- src/metrics_sender/mod.rs
+++ src/metrics_sender/mod.rs
@@ -1,6 +1,7 @@
 use transit::udp::*;
 use std::net::{ToSocketAddrs};
 use std::error::Error;
+use std::fmt;
 
 use time::{get_time};
 use proc_fs::stats::{ProcStatm};
@@ -26,16 +27,50 @@ pub struct MetricSender {
     transit: Transit,
 }
 
-impl MetricSender {
-    pub fn new<A>(addr: A, origin: String) -> MetricSender where A: ToSocketAddrs {
-        let transit = Transit::new(addr).unwrap();
-        MetricSender {
-            origin: origin,
-            transit: transit,
+#[derive(Debug)]
+pub enum MetricError {
+    UDPError(Box<Error + Sync + Send>),
+}
+
+impl From<TransitError> for MetricError {
+    fn from(err: TransitError) -> MetricError {
+        MetricError::UDPError(Box::new(err))
+    }
+}
+
+impl Error for MetricError {
+    fn description(&self) -> &str {
+        match *self {
+            MetricError::UDPError(ref err) => err.description(),
         }
     }
 
-    pub fn send_to<A>(&mut self, data: Metric, addr: A) -> Result<(), TransitError> where A: ToSocketAddrs {
+    fn cause(&self) -> Option<&Error> {
+        match *self {
+            MetricError::UDPError(ref err) => err.cause(),
+        }
+    }
+}
+
+impl fmt::Display for MetricError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MetricError::UDPError(ref err) =>
+                write!(fmt, "UDPError: {}", err),
+        }
+    }
+}
+
+impl MetricSender {
+    pub fn new<A>(addr: A, origin: String) -> Result<MetricSender, MetricError> where A: ToSocketAddrs {
+        let transit = try!(Transit::new(addr));
+        Ok(MetricSender {
+            origin: origin,
+            transit: transit,
+        })
+    }
+
+    pub fn send_to<A>(&mut self, data: Metric, addr: A) -> Result<(), MetricError> where A: ToSocketAddrs {
         // let address = try!(self.transit.local_addr());
         let time = get_time().sec;
         let pkt = MetricPacket {
@@ -44,7 +79,8 @@ impl MetricSender {
             // ip: address.ip(),
             data: data,
         };
-        self.transit.send_to(&pkt, addr)
+        try!(self.transit.send_to(&pkt, addr));
+        Ok(())
     }
 }
 
@@ -58,7 +94,7 @@ mod test {
     fn test_send_to() {
         let metric_addr = "127.0.0.1:60000";
         let listen_addr = "127.0.0.1:60001";
-        let mut metric_sender = MetricSender::new(metric_addr, String::from("test-sender"));
+        let mut metric_sender = MetricSender::new(metric_addr, String::from("test-sender")).unwrap();
         let mut listener = Transit::new(listen_addr).unwrap();
 
         let data = Metric::Process(process_statm(String::from("self")).unwrap());
